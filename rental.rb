e5d4751400ce560e81762e0f74fbb93320af7da0
tsheporamantso <tshepo.ramantso@outlook.com>
2023-08-30 19:03:29 +0200

    Create a belong to relation between rental class and person class

--- rental.rb
+++ rental.rb
@@ -1,11 +1,14 @@
 require './book'
+require './person'
 
 class Rental
-  attr_accessor :date, :book
+  attr_accessor :date, :book, :person
 
-  def initialize(date, book)
+  def initialize(date, book, person)
     @date = date
     @book = book
     book.rentals << self
+    @person = person
+    person.rentals << self
   end
 end
