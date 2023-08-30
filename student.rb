d3c05cabd02bdbe21336c1e6fffec6722db23a99
tsheporamantso <tshepo.ramantso@outlook.com>
2023-08-30 19:14:05 +0200

    Add classroom attribute accessor

--- student.rb
+++ student.rb
@@ -2,6 +2,8 @@ require './person'
 require './classroom'
 
 class Student < Person
+  attr_accessor :classroom
+  
   def initialize(classroom, age, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
