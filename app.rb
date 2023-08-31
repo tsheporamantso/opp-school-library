d4ce5173ac3881e39d98fb2715d1453d00c7f864
tsheporamantso <tshepo.ramantso@outlook.com>
2023-08-31 11:26:27 +0200

    Add method to list all books

--- app.rb
+++ app.rb
@@ -11,4 +11,14 @@ class App
     @rentals = []
     @books = []
   end
+
+  def book_list
+    if @books.empty?
+      puts 'There is no book in the list'
+    else
+      @books.each_with_index do |book, index|
+        puts "#{index} - Title: #{book.title.capitalize}, Author: #{book.author.capitalize}"
+      end
+    end
+  end
 end
