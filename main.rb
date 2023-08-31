f32dd3cd83e940deff6d9e07494f5b34d0fa580b
tsheporamantso <tshepo.ramantso@outlook.com>
2023-08-31 12:29:02 +0200

    Add loop to prompt user option to terminate the game

--- main.rb
+++ main.rb
@@ -2,6 +2,21 @@ require './app'
 
 def main
   app = App.new
+
+  puts 'Welcome to school Library App!'
+  puts ' '
+
+  loop do
+    display_menu
+    option = gets.chomp
+
+    if option == '7'
+      puts 'Thank you for using this app!'
+      break
+    end
+
+    options(option,app)
+  end
 end
 
 def options(option, app)
