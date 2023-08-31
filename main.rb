a8ec7fd227181e3be74dcb2899e7dba0582a6f43
tsheporamantso <tshepo.ramantso@outlook.com>
2023-08-31 12:23:07 +0200

    Fix robocop offences

--- main.rb
+++ main.rb
@@ -2,33 +2,33 @@ require './app'
 
 def main
   app = App.new
+end
 
-  def options(option, app)
-    case option
-    when '1' then app.book.list
-    when '2' then app.people.list
-    when '3' then app.person_create
-    when '4' then app.new_book
-    when '5' then app.new_rental
-    when '6' then app.rental_list
-    else
-      puts 'This is not a valid option'
-    end
+def options(option, app)
+  case option
+  when '1' then app.book.list
+  when '2' then app.people.list
+  when '3' then app.person_create
+  when '4' then app.new_book
+  when '5' then app.new_rental
+  when '6' then app.rental_list
+  else
+    puts 'This is not a valid option'
   end
+end
 
-  def display_menu
-    puts ''
-    puts 'Please choose an option by entering a number'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-    puts ''
-    print 'Enter number: '
-  end
+def display_menu
+  puts ''
+  puts 'Please choose an option by entering a number'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+  puts ''
+  print 'Enter number: '
 end
 
 main
